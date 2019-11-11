c37cb8eac935e5baf572542d76e5ed77308f913c
Zoe <hi@zoe.im>
2019-11-11 15:48:46 +0800

    :tada: init release

---
+++ test/taobao_page_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:taobao_page/taobao_page.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = TaobaoPage();
+  });
+}
