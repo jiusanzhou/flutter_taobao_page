340be253cc614a12b9e89a713d28c8fb309bfcfa
Zoe <jsz3@live.com>
2019-11-26 20:40:40 +0800

    :tada: fix package name

--- test/taobao_page_test.dart
+++ test/taobao_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:taobao_page/taobao_page.dart';
+import 'package:flutter_taobao_page/taobao_page.dart';
 
 void main() {
   test('adds one to input values', () {
